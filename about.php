56fc66c848f12ea65a4660a32d8a75ada6150d80
unknown <armandodiazer@gmail.com>
2021-01-08 18:42:26 +0700

    UPDATE TERBARU

---
+++ about.php
@@ -0,0 +1,106 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ATIGA</title>
+        <link rel="stylesheet" href="StyleAbout.css">
+        <link rel="stylesheet" href="ResponsiveAbout.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h2>WELCOME TO ATIGA WEBSITE<span>.</span></h2>
+            </div>
+        </header>
+        <div class="top-wrapper">
+            <div class="container-wrapper">
+                <h1>ABOUT US</h1>
+            </div>
+        </div>
+        <div class="top-profile">
+            <div class="container">
+                <div class="photo left">
+                    <img src="Asset/Alip.jfif">
+                </div>
+                <div class="top left">
+                    <h2>Alifian Fajar Pratama</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque aut sed error iure quibusdam architecto aliquam, quod eligendi voluptates rerum perspiciatis excepturi vero saepe ratione non sit cumque, necessitatibus alias.</p>
+                    <div class="social-kiri left">
+                        <a href="https://www.instagram.com/alifianfajar123/?hl=en" class="btn instagram"><span class="fa fa-instagram"></span>Instagram</a>
+                    </div>
+                    <div class="social-kiri left">
+                        <a href="https://www.facebook.com/alifian.fajarpratama" class="btn facebook"><span class="fa fa-facebook"></span>Facebook</a>
+                    </div>
+                    <div class="social-kiri left">
+                        <a href="https://github.com/alifdoll" class="btn github"><span class="fa fa-github"></span>Github</a>
+                    </div>
+                </div>
+            </div>          
+        </div>
+        <div class="mid-profile">
+            <div class="container">
+                <div class="mid right">
+                    <h2>Azriel Rizky</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque aut sed error iure quibusdam architecto aliquam, quod eligendi voluptates rerum perspiciatis excepturi vero saepe ratione non sit cumque, necessitatibus alias.</p>
+                    <div class="social-kanan right">
+                        <a href="https://www.instagram.com/azrielrzky/?hl=en" class="btn instagram"><span class="fa fa-instagram"></span>Instagram</a>
+                    </div>
+                    <div class="social-kanan right">
+                        <a href="https://www.facebook.com/azriel.ok.3" class="btn facebook"><span class="fa fa-facebook"></span>Facebook</a>
+                    </div>
+                    <div class="social-kanan right">
+                        <a href="https://github.com/Azrielrzky" class="btn github"><span class="fa fa-github"></span>Github</a>
+                    </div>
+                </div>
+                <div class="photo right">
+                    <img src="Asset/Azriel.jpeg">
+                </div>
+            </div>
+        </div>
+        <div class="bottom-profile">
+            <div class="container">
+                <div class="photo left">
+                    <img src="Asset/Armando.jpg">
+                </div>
+                <div class="bottom left">
+                    <h2>Armando Diaz</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque aut sed error iure quibusdam architecto aliquam, quod eligendi voluptates rerum perspiciatis excepturi vero saepe ratione non sit cumque, necessitatibus alias.</p>
+                    <div class="social-kiri left">
+                        <a href="https://www.instagram.com/armando.diazer/?hl=en" class="btn instagram"><span class="fa fa-instagram"></span>Instagram</a>
+                    </div>
+                    <div class="social-kiri left">
+                        <a href="https://www.facebook.com/armando.diaz.942145/" class="btn facebook"><span class="fa fa-facebook"></span>Facebook</a>
+                    </div>
+                    <div class="social-kiri left">
+                        <a href="https://github.com/ArmandoZaid" class="btn github"><span class="fa fa-github"></span>Github</a>
+                    </div>
+                </div>
+            </div>          
+        </div>
+        <div class="desc">
+            <div class="container">
+                <div class="decontainer">
+                    <h1>ATIGA<span>.<span></h1>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem modi, unde voluptates ab placeat expedita necessitatibus quam eos odio similique corporis quidem amet, repudiandae eligendi velit nostrum, eius rem porro?</p>
+                <div class="des decontainer">
+                    <h1>VISI</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nihil aliquam nesciunt optio repellendus consectetur, quis laudantium hic culpa quo saepe ex provident architecto ipsum dolorem qui dolorum eligendi ad!</p>
+                </div>
+                <div class="des decontainer">
+                    <h1>MISI</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur assumenda animi corrupti repellendus placeat sit odit corporis ipsam. Quaerat at unde blanditiis quae debitis quibusdam beatae vitae ipsum minima!</p>
+                </div>
+                <div class="decontainer">
+                    <a href="index.php" class="btn-home">HOME</a>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="container">
+                <h1>ATIGA<span>.</span></h1>
+                <p>@2021 Powered by ATIGA Team</p>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ATIGA</title>
+        <link rel="stylesheet" href="StyleIndex.css">
+        <link rel="stylesheet" href="ResponsiveIndex.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h2>WELCOME TO ATIGA WEBSITE<span>.</span></h2>
+            </div>
+        </header>
+        <div class="top-wrapper">
+            <div class="container">
+                <h1>YOU ARE NOT ALONE</h1>
+                <h1>WE ARE HERE FOR YOU</h1>
+            </div>
+        </div>
+        <div class="content">
+            <div class="list-container">
+                <div class="list-content">
+                    <a href="index.php" class="btn home">HOME</a>
+                </div>
+                <div class="list-content">
+                    <a href="#" class="btn news">NEWS</a>
+                </div>
+                <div class="list-content">
+                    <a href="about.php" class="btn about">ABOUT US</a>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+                <div class="container">
+                    <div class="items">
+                        <div class="list-item">
+                            <div class="list-items">
+                                <img src="https://i.pinimg.com/564x/ba/b5/ab/bab5abd5f2206dd1156c70c8e36d8354.jpg">
+                                <h2>JEPANG ITU INDAH</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, deserunt? Facilis beatae aspernatur ratione ducimus in ex voluptatem velit repudiandae recusandae pariatur exercitationem maxime, dignissimos nemo aperiam adipisci modi iste.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="items">
+                        <div class="list-item">
+                            <div class="list-items">
+                                <img src="https://i.pinimg.com/564x/ba/b5/ab/bab5abd5f2206dd1156c70c8e36d8354.jpg">
+                                <h2>JEPANG ITU INDAH</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, deserunt? Facilis beatae aspernatur ratione ducimus in ex voluptatem velit repudiandae recusandae pariatur exercitationem maxime, dignissimos nemo aperiam adipisci modi iste.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="items">
+                        <div class="list-item">
+                            <div class="list-items">
+                                <img src="https://i.pinimg.com/564x/ba/b5/ab/bab5abd5f2206dd1156c70c8e36d8354.jpg">
+                                <h2>JEPANG ITU INDAH</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, deserunt? Facilis beatae aspernatur ratione ducimus in ex voluptatem velit repudiandae recusandae pariatur exercitationem maxime, dignissimos nemo aperiam adipisci modi iste.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="items">
+                        <div class="list-item">
+                            <div class="list-items">
+                                <img src="https://i.pinimg.com/564x/ba/b5/ab/bab5abd5f2206dd1156c70c8e36d8354.jpg">
+                                <h2>JEPANG ITU INDAH</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, deserunt? Facilis beatae aspernatur ratione ducimus in ex voluptatem velit repudiandae recusandae pariatur exercitationem maxime, dignissimos nemo aperiam adipisci modi iste.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="items">
+                        <div class="list-item">
+                            <div class="list-items">
+                                <img src="https://i.pinimg.com/564x/ba/b5/ab/bab5abd5f2206dd1156c70c8e36d8354.jpg">
+                                <h2>JEPANG ITU INDAH</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, deserunt? Facilis beatae aspernatur ratione ducimus in ex voluptatem velit repudiandae recusandae pariatur exercitationem maxime, dignissimos nemo aperiam adipisci modi iste.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="items">
+                        <div class="list-item">
+                            <div class="list-items">
+                                <img src="https://i.pinimg.com/564x/ba/b5/ab/bab5abd5f2206dd1156c70c8e36d8354.jpg">
+                                <h2>JEPANG ITU INDAH</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, deserunt? Facilis beatae aspernatur ratione ducimus in ex voluptatem velit repudiandae recusandae pariatur exercitationem maxime, dignissimos nemo aperiam adipisci modi iste.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="btn-lihat">LIHAT LAINYA</a>
+                </div>
+            </div>
+        <footer>
+            <div class="container">
+                <h1>ATIGA<span>.</span></h1>
+                <p>@2021 Powered by ATIGA Team</p>
+            </div>
+        </footer>
+    </body>
+</html>
