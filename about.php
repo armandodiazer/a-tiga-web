24856b6afb3a7359974df565d56a39ebbd6afdf3
alif <alifianfajarpratama@gmail.com>
2021-01-09 07:10:48 +0700

    Cleaning

--- about.php
+++ about.php
@@ -2,8 +2,8 @@
     <head>
         <meta charset="utf-8">
         <title>ATIGA</title>
-        <link rel="stylesheet" href="StyleAbout.css">
-        <link rel="stylesheet" href="ResponsiveAbout.css">
+        <link rel="stylesheet" href="Style/StyleAbout.css">
+        <link rel="stylesheet" href="Style/ResponsiveAbout.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     </head>
     <body>
